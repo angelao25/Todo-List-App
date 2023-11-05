95527071e2e13457d557f89624ad28975583fe06
Angel Aguirre Obando <anngelao25@gmail.com>
2023-11-05 00:11:59 -0500

    Added Turbo Frame

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,26 @@
 class TasksController < ApplicationController
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successfully updated"}
+      else
+        format.html { render :edit, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "Post was successfully deleted."
+  end
+
+
   def create
     @task = Task.new(task_params)
 
@@ -14,6 +36,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
     @task = Task.new
+  end
+
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: params[:completed])
+
+    render json: { message: "Success" }
   end
 
   private
